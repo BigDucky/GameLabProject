6c454490f7e324b48295354bfc2dcaad691ff3ba
BigDucky <atungluong@hotmail.com>
2018-02-25 15:41:38 +0100

    Placing Building ( Done ) Also Types of Building Done

--- GameLabProject/Assets/Scripts/GameLogic.cs
+++ GameLabProject/Assets/Scripts/GameLogic.cs
@@ -4,16 +4,13 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "GameSettings",menuName = "Settings")]
 public class GameLogic : ScriptableObject {
-    public List<GameObject> buildings = new List<GameObject>();
-    public static List<GameObject> Obuildings = new List<GameObject>(); // Accesable for every script
+    public List<GameObject> Obuildings = new List<GameObject>(); 
 	// Use this for initialization
 	void Start () {
-        Obuildings = buildings;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log(buildings);
-        Debug.Log(Obuildings);
+
     }
 }
--- GameLabProject/Assets/Scripts/Player.cs
+++ GameLabProject/Assets/Scripts/Player.cs
@@ -6,31 +6,45 @@ public class Player : MonoBehaviour {
     public bool isPlacing = false;
     private List<GameObject> buildingList = new List<GameObject>();
     public RaycastHit hit;
+    public GameLogic gameLogic;
+    private GameObject tempBuilding;
+    private int BuildingType;
 	// Use this for initialization
 	void Start () {
-        buildingList = GameLogic.Obuildings;
+        buildingList = gameLogic.Obuildings;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        PlayerInteractions();
-
+        if(isPlacing) {
+            PlayerInteractions(BuildingType);
+            tempBuilding.transform.position = Input.mousePosition;
+        }
 	}
 
     /// <summary>
     /// Checks which tile is clicked
     /// </summary>
-    void PlayerInteractions() {
-        if (Input.GetMouseButton(0)) {
+    /// 
+    void PlayerInteractions(int buildingType) {
+        if (Input.GetMouseButtonDown(0)) {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if(Physics.Raycast(ray,out hit, 100f)) {
-                PlaceBuilding();
+                PlaceBuilding(buildingType);
+                isPlacing = false;
             }
         }
     }
 
-    void PlaceBuilding() {
-        Instantiate(buildingList[0], hit.transform.position, Quaternion.identity);
+    void PlaceBuilding(int buildingType) {
+        Instantiate(buildingList[buildingType], hit.transform.position, Quaternion.identity);
+    }
+
+    public void SelectBuilding(int type) {
+        isPlacing = true;
+        BuildingType = type;
+        tempBuilding = buildingList[type];
+        Instantiate(tempBuilding);
     }
 
 }
