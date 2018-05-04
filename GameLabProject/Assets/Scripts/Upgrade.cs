7cb2e0bc6a399b2be579df0b6bc1c508c1a7d88d
BigDucky <atungluong@hotmail.com>
2018-05-04 12:22:33 +0200

    Upgrades Added Data

---
+++ GameLabProject/Assets/Scripts/Upgrade.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrade : MonoBehaviour {
+
+    public List<BuildingData> upgrades = new List<BuildingData>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void LevelUpBuilding() {
+
+    }
+}
