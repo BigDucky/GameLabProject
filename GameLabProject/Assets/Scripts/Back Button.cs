acceb011a769e73f2185111782a791fee82ac4bc
RubjeMazter <rubengeerts@hotmail.com>
2018-05-15 14:09:36 +0200

    Minor changes to Pause script, added back button script

---
+++ GameLabProject/Assets/Scripts/Back Button.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackButton : MonoBehaviour {
+
+	public GameObject optionsMenu;
+	public GameObject mainScreen;
+	public static bool isOptionsMenu = false; 
+
+	// Update is called once per frame
+	void Update () {
+		onClick ();
+	}
+
+	void onClick()
+	{
+		if (isOptionsMenu)
+		{
+			closeOptions ();
+		} else
+		{
+			openOptions ();
+		}
+	}
+
+	void openOptions()
+	{
+		optionsMenu.SetActive (true);
+		mainScreen.SetActive (false);
+		isOptionsMenu = true;
+	}
+
+	void closeOptions()
+	{
+		mainScreen.SetActive (true);
+		optionsMenu.SetActive (false);
+		isOptionsMenu = false;
+	}
+
+}
