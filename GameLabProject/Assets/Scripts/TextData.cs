615d365045e559f3891a14f7af16878b9c8616e7
BigDucky <atungluong@hotmail.com>
2018-05-19 14:55:41 +0200

    File Reset

--- GameLabProject/Assets/Scripts/TextData.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "DialogData", menuName = "DialogData")]
-public class TextData : ScriptableObject {
-
-    public List<string> text = new List<string>();
-}
