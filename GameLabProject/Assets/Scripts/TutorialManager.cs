873d844133ca3cc14ce5376e16fe1e7aff01cff1
RubjeMazter <rubengeerts@hotmail.com>
2018-06-19 11:40:46 +0200

    Revert "Merge branch 'master' of https://github.com/BigDucky/GameLabProject"
    This reverts commit 06b8fb72a427cdd378bcf5f4ef23b98a67ade46b, reversing
    changes made to 281c6595ee29c92f7a82e3ee02264c0c8f364d7b.

--- GameLabProject/Assets/Scripts/TutorialManager.cs
+++ GameLabProject/Assets/Scripts/TutorialManager.cs
@@ -14,9 +14,6 @@ public class TutorialManager : MonoBehaviour {
     public bool buildButtonClick;
     public static bool build;
     public static bool click;
-	public bool mineProduce;
-	public bool upgradeButton;
-	public bool techButton;
 
     public bool waitingForPlayer;
     public string playerActionNeeded;
@@ -48,21 +45,9 @@ public class TutorialManager : MonoBehaviour {
         }*/
     }
 
-	public void EnableTechButton(){
-		techButton = true;
-		Invoke("ResetBool", 1);
-	
-	}
-
-	public void EnableUpgradeButton(){
-		upgradeButton = true;
-		Invoke("ResetBool", 1);
-
-	}
-
     public void EnableBuildCheck() {
         buildButtonClick = true;
-		Invoke("ResetBool", 1);
+        Invoke("ResetBool", 1);
     }
 
     public void EnableSelectedBuildckeck() {
@@ -70,17 +55,9 @@ public class TutorialManager : MonoBehaviour {
         Invoke("ResetBool", 1);
     }
 
-	public void EnableMineProduceCheck(){
-		mineProduce = true;
-		Invoke ("ResetBool", 1);
-	}
-
     void ResetBool() {
         buildButtonClick = false;
         selectedBuilding = false;
-		mineProduce = false;
-		techButton = false;
-
     }
 
     void EndTutorial() {
@@ -134,31 +111,9 @@ public class TutorialManager : MonoBehaviour {
                     nextTutorialStep();
                 }
             break;
-		case "MINEPRODUCE":
-			if (mineProduce) {
-				waitingForPlayer = false;
-				nextTutorialStep ();
-			}
-			break;
-		case "TECHBUTTON":
-			if (techButton) {
-				waitingForPlayer = false;
-				nextTutorialStep ();
-			}
-			break;
 
-		case "UPGRADEBUTTON":
-			if (upgradeButton) {
-				waitingForPlayer = false;
-				nextTutorialStep ();
-			}
-			break;
             default:
-<<<<<<< HEAD
                 UiManager.outOfTutorial = true;
-=======
-			
->>>>>>> 9f9030e1a27bc0b978209ed4b7e5f57f0845c4cc
 
                 break;
         }
