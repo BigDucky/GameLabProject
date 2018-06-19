a8101fd727826f91c7fa2bd598e2ce2216739ac6
BigDucky <atungluong@hotmail.com>
2018-06-19 12:05:17 +0200

    Delte

--- GameLabProject/Assets/Scripts/TextData.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "DialogData", menuName = "DialogData")]
-public class TextData : ScriptableObject {
-    [TextArea]
-    public List<string> text = new List<string>();
-}
